2d37be08801b27aa5482a3947f1038d798ce6b79
Franco Lopez Paviolo <francomlopez@mi.unc.edu.ar>
2022-04-24 17:26:13 -0300

    Finished testbench for decode

--- mips_final.srcs/sim_1/new/decode_tb.sv
+++ mips_final.srcs/sim_1/new/decode_tb.sv
@@ -1,36 +1,47 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/23/2022 07:42:46 PM
-// Design Name: 
-// Module Name: decode_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
+`define assert(signal, value) \
+        if (signal !== value) begin \
+            $display("ASSERTION FAILED in %m: %b != %b",signal, value ); \
+            $finish; \
+        end
 
 module decode_tb();
-logic regWrite, clk;
-logic [31:0] wd, instr, signExt, readData1, readData2;
+logic regWrite, clk = 1;
+logic [31:0] writeData3, instr, signExt, readData1, readData2;
 logic [4:0] wa3;
 		
-decode dec(regWrite, clk, wd, instr, signExt, 
-readData1, readData2, wa3);
-
+decode dec(.regWrite(regWrite),.clk(clk),.wa3(wa3),.writeData3(writeData3),.instr(instr),
+.signExt(signExt), .readData1(readData1),.readData2(readData2));
+	
  initial begin
-    
+		$display("Starting test...");
+		
+		wa3 = 5'b00000; #5;
+		writeData3 = 32'b0011; #5;
+		regWrite = 1; #40;
+        regWrite = 0; #5;
+        
+ 		wa3 = 5'b00001; #5;
+		writeData3 = 32'b1100; #5;
+		regWrite = 1; #40;
+		regWrite = 0; #5;
+        instr = 32'b0000_0000_0000_0001_0000_0000_0000_0000; #5;
+        
+		`assert(readData1, 32'b0011)
+		`assert(readData2, 32'b1100)
+		
+		instr = 32'b0000_0000_0000_0001_1000_0000_1111_0000; #5;
+		
+		`assert(signExt, 32'b1111_1111_1111_1111_1000_0000_1111_0000)
+		
+		instr = 32'b0010_0000_1111_0100_0000_0000_1111_0000; #5;
+		
+		`assert(signExt, 32'b1111_0000)
+		
+		$display("TEST PASSED SUCCESFULLY");
+		$finish;
     end
 
-
+    always 
+       #20 clk = ~clk;
+   
 endmodule
